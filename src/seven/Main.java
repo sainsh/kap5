56be61f52c26e217fce6e056c33703e8c739f6d8
sainsh <kfsainsh@gmail.com>
2018-10-02 11:50:41 +0200

    seven

---
+++ src/seven/Main.java
@@ -0,0 +1,33 @@
+package seven;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int rate = 5;
+        double tuition10 = 10000.0;
+        double cost = 0;
+
+        for(int i=0; i<10; i++){
+
+
+
+            tuition10 += tuition10/100*rate;
+
+        }
+        System.out.println("Tuition after 10 years: " + tuition10);
+        cost = tuition10;
+        for (int j=0; j<4; j++){
+
+
+            cost += tuition10 + tuition10/100*rate;
+
+        }
+
+
+        System.out.println("Total cost for 4 years after 10 years: " + cost );
+
+    }
+
+}
