499c60c1c07d8a8cffd88260662dd88f2b144364
sainsh <kfsainsh@gmail.com>
2018-10-02 13:37:58 +0200

    ten + eleven

---
+++ src/ten/Main.java
@@ -0,0 +1,23 @@
+package ten;
+
+public class Main {
+    public static void main(String[] args) {
+        int count=0;
+        String output = "";
+
+        for(int i = 100; i<1001; i++){
+
+            if((i % 5 == 0) && (i % 6 == 0)){
+                output += i + " ";
+                count++;
+                if(count % 10 == 0){
+                    output += "\n";
+                }
+            }
+
+        }
+        System.out.println(output);
+
+    }
+
+}
